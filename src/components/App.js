7d58c0c364fa0d26005860ccceb8b068a101fcca
Harold Seo <harold.seo.612@gmail.com>
2022-01-24 15:37:02 -0800

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TestElements from "./TestElements/TestElements";
+
+const App = () => {
+  return (
+    <div>
+      <TestElements />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TestElements/TestElements.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const TestElements = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <h1 data-testid='counter'>{counter}</h1>
+      <button data-testid='button-up' onClick={() => setCounter(counter + 1)}>
+        {" "}
+        Up
+      </button>
+      <button disabled data-testid='button-down' onClick={() => setCounter(counter - 1)}>
+        Down
+      </button>
+    </>
+  );
+};
+
+export default TestElements;
