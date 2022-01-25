aaaae32fb3f0eced56de394a2c14132915a1c700
Harold Seo <harold.seo.612@gmail.com>
2022-01-24 21:07:07 -0800

    Add TestEvents Component

--- src/components/TestEvents/TestEvents.js
+++ src/components/TestEvents/TestEvents.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const TestElements = () => {
+const TestEvents = () => {
   const [counter, setCounter] = useState(0);
 
   return (
@@ -9,11 +9,11 @@ const TestElements = () => {
       <button data-testid='button-up' onClick={() => setCounter(counter + 1)}>
         Up
       </button>
-      <button disabled data-testid='button-down' onClick={() => setCounter(counter - 1)}>
+      <button data-testid='button-down' onClick={() => setCounter(counter - 1)}>
         Down
       </button>
     </div>
   );
 };
 
-export default TestElements;
+export default TestEvents;
